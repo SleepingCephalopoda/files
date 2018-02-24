03a7d649342c6689a5173cadc2a01316adaf00ab
sleepingcephalopoda <sleepingcephalopoda@users.noreply.github.com>
2018-02-24 13:20:06 -0400

    Readibility changes.

--- Text.cpp
+++ Text.cpp
@@ -7,14 +7,13 @@ using namespace std;
 
 int main( )
 {
-   // Create data.
-   short int a = -6730;
-   float b = 68.123; 
-   char c = 'J';
+	// Create data.
+	short int a = -6730;
+	float b = 68.123;
+	char c = 'J';
  
 	// Display data to console as formatted characters.
 	cout << a << " " << b << " " << c;
-
 	cout << endl << endl;
 	
 	// Try to open abc.txt for writing output.
