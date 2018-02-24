9160cc027d749c80c8bee8774416b494ab5a91fe
sleepingcephalopoda <sleepingcephalopoda@users.noreply.github.com>
2018-02-24 10:58:37 -0400

    Text.cpp file created as a dulicate of screen.cpp

---
+++ Text.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+	// Display data to console as formatted characters.
+	cout<< a << " " << b << " " << c;
+
+	cout << endl << endl;
+ 
+   return 0;
+}
