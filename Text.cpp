f75db61ddef698680e1fc44659533c7ec87e5ce4
sleepingcephalopoda <sleepingcephalopoda@users.noreply.github.com>
2018-02-24 12:53:32 -0400

    Modified to write varaibles to text file. Text file generated.

--- Text.cpp
+++ Text.cpp
@@ -1,17 +1,43 @@
+// For keyboard and console I/O.
 #include <iostream>
+// For file I/O.
+#include <fstream>
+
 using namespace std;
 
 int main( )
 {
-   //create some data
+   // Create data.
    short int a = -6730;
    float b = 68.123; 
    char c = 'J';
  
 	// Display data to console as formatted characters.
-	cout<< a << " " << b << " " << c;
+	cout << a << " " << b << " " << c;
 
 	cout << endl << endl;
+	
+	// Try to open abc.txt for writing output.
+	ofstream myfile {"abc.txt"};
+	
+	// Check if file opened successfully.
+	if (myfile)
+	
+	{
+		// Write data to text file.
+		myfile << a << " " << b << " " << c;
+	}
+    
+    else
+    
+    {
+		// Print error before program quits.
+		cout<<"Error opening abc.txt" << endl;
+	}
+	
+	// Optional in this case - destructor will close it automatically.
+	myfile.close();
  
-   return 0;
+	return 0;
+	
 }
